0e01b5acf5854cc5e6b53504618beb9a6517b32e
Andrew Switlyk <andrew.switlyk@macys.com>
2015-01-06 13:19:07 -0800

    Add requireJS AMD support to Jasmine testing. Now load test suites as modules within SpecRunner.html's require statement

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 /**************  RequireJS configuration   ************/
 
-requirejs.config({
-
-  // default baseUrl will be src/ since data-main entry point
-  // is src/index.js
-  // baseUrl: 'src/',
-
-  // shim configurations here
-  shim: {
-    'backbone': {
-      // these script dependencies should be loaded before
-      // loading backbone.js
-      deps: ['underscore', 'jquery'],
-      // Once loaded, use the global 'Backbone' as the
-      // module value
-      exports: 'Backbone'
-    },
-    'underscore': {
-      exports: '_'
-    },
-    'jquery': {
-      deps: [],
-      exports: '$'
-    },
-    'template': {
-      deps: ['handlebars']
-    }
-  },
-
-
-  paths: {
-    jquery: '../node_modules/jquery/dist/jquery',
-    underscore: '../node_modules/underscore/underscore',
-    backbone: '../node_modules/backbone/backbone',
-    handlebars: '../node_modules/handlebars/dist/handlebars',
-    'handlebars.runtime': '../node_modules/handlebars/dist/handlebars.runtime',
-    template: './compiledTemplates/template'
-  }
-
-});
+// requirejs.config({
+//
+//   // default baseUrl will be src/ since data-main entry point
+//   // is src/index.js
+//   // baseUrl: 'src/',
+//
+//   // shim configurations here
+//   shim: {
+//     'backbone': {
+//       // these script dependencies should be loaded before
+//       // loading backbone.js
+//       deps: ['underscore', 'jquery'],
+//       // Once loaded, use the global 'Backbone' as the
+//       // module value
+//       exports: 'Backbone'
+//     },
+//     'underscore': {
+//       exports: '_'
+//     },
+//     'jquery': {
+//       deps: [],
+//       exports: '$'
+//     },
+//     'template': {
+//       deps: ['handlebars']
+//     }
+//   },
+//
+//
+//   paths: {
+//     jquery: '../node_modules/jquery/dist/jquery',
+//     underscore: '../node_modules/underscore/underscore',
+//     backbone: '../node_modules/backbone/backbone',
+//     handlebars: '../node_modules/handlebars/dist/handlebars',
+//     'handlebars.runtime': '../node_modules/handlebars/dist/handlebars.runtime',
+//     template: './compiledTemplates/template'
+//   }
+//
+// });
 
 define(['jquery', 'underscore', 'backbone', 'template'], function($, _, backbone, template) {
 
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,5 +1,24 @@
-describe("testFun()", function() {
-  it("should return true when called", function() {
-    expect(testFun()).toBe(true);
+describe("Load RequireJS modules using Jasmine 2.0 async", function() {
+
+  // Each spec within this suite shares the same "this" object
+
+  beforeEach(function(done) {
+
+    // point that to shared this object
+    var that = this;
+
+    require(['jquery', 'backbone'], function($, backbone) {
+      // share AMD loaded variables
+      that.$ = $;
+      that.Backbone = backbone;
+      done();
+    });
+
   });
+
+  it("Should load dependencies", function() {
+    expect(this.$).toBeDefined();
+    expect(this.Backbone).toBeDefined();
+  });
+
 });
